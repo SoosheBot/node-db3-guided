e12a13da96c6c795f2e2aa57f15aac66aa76b975
sooshe <sooshe@sooshebot.com>
2019-12-11 12:25:29 -0600

    add more queries

--- queries.sql
+++ queries.sql
@@ -10,13 +10,15 @@ join employee as e
 join customer as c 
     on o.CustomerId = c.id;
 
+--Another way to order 
 select p.ProductName, od.Quantity, p.UnitPrice, * from [Order] as o
 join OrderDetail as od
     on o.id = od.OrderId
 join product as p
     on od.productId = p.id;
 
-    select o.id as OrderNumber
+--join product unit price as Price 
+select o.id as OrderNumber
 , p.ProductName
 , od.Quantity
 , p.UnitPrice as Price
@@ -26,3 +28,33 @@ join orderdetail as od
     on o.id = od.OrderId
 join product as p
     on od.ProductId = p.Id;
+
+--join supplier company name as SoldBy
+select o.id as OrderNumber
+, p.ProductName
+, od.Quantity
+, p.UnitPrice as Price
+, (od.Quantity * p.UnitPrice) as LineTotal
+, s.CompanyName as SoldBy
+from [order] as o
+join orderdetail as od
+    on o.id = od.OrderId
+join product as p
+    on od.ProductId = p.Id
+join supplier as s
+    on s.Id = p.SupplierId;
+
+select distinct c.id, c.CompanyName
+from customer as c
+join [order] as o on c.id = o.customerId
+order by c.id; --16589 (90 unique)
+
+-- all the customer even if they have no order
+select c.id, c.companyName as Customer, o.Id
+from customer as c
+left join [order] as o on c.id = o.customerId
+order by c.id;
+
+select * from [order]
+-- delete from [order]
+where customerId = 'ALFKI'
